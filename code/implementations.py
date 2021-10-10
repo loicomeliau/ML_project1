24c03a858347868993b267eca5c9d75deaccdc96
Loïc Comeliau <loic.comeliau@gmail.com>
2021-10-10 13:39:06 +0200

    GD and SGD

--- code/implementations.py
+++ code/implementations.py
@@ -1,8 +1,10 @@
 import numpy as np
 
-#Functions for the calculation of the loss
+
+# Functions for the calculation of the loss
 def calculate_mse(e):
-    return 1/2*np.mean(e**2)
+    return 1 / 2 * np.mean(e ** 2)
+
 
 def calculate_mae(e):
     return np.mean(np.abs(e))
@@ -12,16 +14,77 @@ def compute_loss(y, tx, w):
     e = y - tx.dot(w)
     return calculate_mse(e)
 
-#Least squares regression using normal equations
+
+# Compute gradient and error vector
+def compute_gradient(y, tx, w):
+    e = y - tx.dot(w)
+    n = len(y)
+
+    gradient = -1 / n * tx.T.dot(e)
+    return gradient, e
+
+
+# Linear regression using gradient descent
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    # Apply gradient descent over max_iters iteration
+    for n_iter in range(max_iters):
+        # Compute gradient and loss
+        gradient, _ = compute_gradient(y, tx, w)
+        loss = compute_loss(y, tx, w)
+        # Update w by gradient
+        w = w - gamma * gradient
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return losses, ws
+
+
+# Linear regression using stochastic gradient descent
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    """Stochastic gradient descent algorithm."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # Pick mini-batch
+        data_size = len(y)
+        sample_index = np.random.randint(0, data_size)
+        minibatch_y = y[sample_index:sample_index+1]
+        minibatch_tx = tx[sample_index:sample_index+1, :]
+        # Compute SG and loss
+        stoch_gradient = compute_gradient(minibatch_y, minibatch_tx, w)
+        loss = compute_loss(y, tx, w)
+        # Update w by stochastic gradient
+        w = w - gamma * stoch_gradient
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+
+        print("Stochastic Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+            bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return losses, ws
+
+
+# Least squares regression using normal equations
 def least_squares(y, tx):
     w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
     loss = compute_loss(y, tx, w)
     return w, loss
 
-#Ridge regression using normal equations
-def ridge_regression(y, tx, lambda_ = 0.1):
-    A = tx.T.dot(tx) + 2*tx.shape[0]*lambda_*np.eye(tx.shape[1])
+
+# Ridge regression using normal equations
+def ridge_regression(y, tx, lambda_=0.1):
+    A = tx.T.dot(tx) + 2 * tx.shape[0] * lambda_ * np.eye(tx.shape[1])
     x = tx.T.dot(y)
-    w = np.linalg.solve(A,x)
+    w = np.linalg.solve(A, x)
     loss = compute_loss(y, tx, w)
     return w, loss
