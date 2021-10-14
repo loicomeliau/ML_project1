6dfaba3591b879ca6a0076fa6829204c5fd9190a
Loïc Comeliau <loic.comeliau@gmail.com>
2021-10-14 10:17:00 +0200

    writes prediction correctly in csv file

--- code/run.py
+++ code/run.py
@@ -1,5 +1,4 @@
 import numpy as np
-# import csv
 
 from implementations import (least_squares_GD, least_squares_SGD,
                              least_squares, ridge_regression,
@@ -8,30 +7,40 @@ from load_data import load_data
 from data_processing import preprocessing, correctness, standardize, \
     build_model_data
 
+
+'''Build model using x method'''
 # Load data
 features, labels = load_data()
-
 # Preprocessing
 train_tx, train_y, test_tx, test_y = preprocessing(features, labels)
+# Model computation using least_squares
 weights, loss = least_squares(train_y, train_tx)
 
-# train_score, test_score = correctness(train_tx, train_y, test_tx, test_y, weights)
-
-# Make prediction from test.csv
+'''Make prediction from test.csv'''
+# Load test features
 test_features, test_prediction = load_data('test')
-idx = np.where(features == -999)[1]
-processed_f = np.delete(features, idx, 1)
-processed_l = labels
-processed_f, _, _ = standardize(processed_f)
-test_y, test_tx = build_model_data(processed_f, processed_l)
-
+# Extract features (manually or reuse preprocessing with ratio=1)
+#idx = np.where(features == -999)[1]
+#processed_f = np.delete(test_features, idx, 1)
+#processed_l = test_prediction
+#processed_f, _, _ = standardize(processed_f)
+#_, test_tx = build_model_data(processed_f, processed_l)
+test_tx, _, _, _ = preprocessing(test_features, test_prediction, ratio=1)
+# Compute prediction
 pred = test_tx.dot(weights)
-pred[pred >= 0.5] = 1
-pred[pred < 0.5] = 0
+# Convert predictions in 's' or 'b'
+idx_b = pred >= 0.5
+idx_s = pred < 0.5
+pred = pred.astype(dtype='|S10')  # Note: dtype='|S10' == output.dtype and allows to replace int by str
+pred[idx_b] = 'b'
+pred[idx_s] = 's'
 
-# Writing in csv file
-# path_csv = "../data/sample-submission.csv"
-# with open('../data/sample-submission.csv', 'w', newline='') as csvfile:
-# ...
-
-
+'''Write predictions in csv file'''
+# Define path of template and output files
+template_file_path = '../data/sample-submission.csv'
+output_file_path = '../data/test_prediction_least_squares.csv'
+# Insert predictions in template
+output = np.genfromtxt(template_file_path, dtype=str, delimiter=',', skip_header=0)
+output[1:, 1] = pred
+# Save output file
+np.savetxt(output_file_path, output, delimiter=",", fmt='%s')
